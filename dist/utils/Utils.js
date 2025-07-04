503c66c80a4d617a15a359caad1f777626148c72
adambor <adamborteam@gmail.com>
2025-07-04 23:20:27 +0200

    Use logger for all logging

--- dist/utils/Utils.js
+++ dist/utils/Utils.js
@@ -6,10 +6,10 @@ const buffer_1 = require("buffer");
 const crypto_1 = require("crypto");
 function getLogger(prefix) {
     return {
-        debug: (msg, ...args) => console.debug(prefix + msg, ...args),
-        info: (msg, ...args) => console.info(prefix + msg, ...args),
-        warn: (msg, ...args) => console.warn(prefix + msg, ...args),
-        error: (msg, ...args) => console.error(prefix + msg, ...args)
+        debug: (msg, ...args) => global.atomiqLogLevel >= 3 && console.debug(prefix + msg, ...args),
+        info: (msg, ...args) => global.atomiqLogLevel >= 2 && console.info(prefix + msg, ...args),
+        warn: (msg, ...args) => (global.atomiqLogLevel == null || global.atomiqLogLevel >= 1) && console.warn(prefix + msg, ...args),
+        error: (msg, ...args) => (global.atomiqLogLevel == null || global.atomiqLogLevel >= 0) && console.error(prefix + msg, ...args)
     };
 }
 exports.getLogger = getLogger;
